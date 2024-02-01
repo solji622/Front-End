1c1ff18a216e2184171ce8341cb4b6f9e3a89aeb
LEE SOLJI <126930370+solji622@users.noreply.github.com>
2024-02-01 11:41:24 +0900

    Update script.js

--- Password Generator/script.js
+++ Password Generator/script.js
@@ -1,4 +1,4 @@
-const lengthSilder = document.querySelector(".pass-length input");
+const lengthSlider = document.querySelector(".pass-length input");
 const options = document.querySelectorAll(".option input");
 const copyIcon = document.querySelector(".input-box span");
 const passwordInput = document.querySelector(".input-box input");
@@ -6,37 +6,70 @@ const passIndicator = document.querySelector(".pass-indicator");
 const generateBtn = document.querySelector(".generate-btn");
 
 const characters = {
-    lowercase: "abcdefghigklmnopqrstuvwxyz",
-    uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    numbers: "0123456789",
-    symbols: "!$%&|[]{}():;.,*+-#@<>~"
-}
+  lowercase: "abcdefghijklmnopqrstuvwxyz",
+  uppercase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  numbers: "0123456789",
+  symbols: "!$%&|[](){}:;.,*+-#@<>~",
+};
 
 const generatePassword = () => {
-    let staticPassword = "",
+  let staticPassword = "",
     randomPassword = "",
     excludeDuplicate = false,
-    passLength = lengthSilder.value;
+    passLength = lengthSlider.value;
 
-    options.forEach(option => {
-        if(option.checked){
-            if(option.id !== "exc-duplicate" && option.id !== "spaces"){
-                staticPassword = characters[option.id];
-            } else if(option.id === "spaces") {
-                staticPassword += `${staticPassword}`;
-            } else {
-                excludeDuplicate = true;
-            }
-        }
-    });
-
-    for(let i = 0; i < passLength; i++){
-        let randomChar = staticPassword[Math.floor(Math.random() * staticPassword.length)];
-        if(excludeDuplicate){
-            !randomPassword.includes(randomChar) | randomChar == "" ? randomPassword += randomChar : i--;
-        } else {
-            randomPassword += randomChar;
-        }
+  options.forEach((option) => {
+    if (option.checked) {
+      if (option.id !== "exc-duplicate" && option.id !== "spaces") {
+        staticPassword += characters[option.id];
+      } else if (option.id === "spaces") {
+        staticPassword += ` ${staticPassword} `;
+      } else {
+        excludeDuplicate = true;
+      }
     }
-    passwordInput.value = randomPassword;
-}
+  });
+
+  for (let i = 0; i < passLength; i++) {
+    let randomChar =
+      staticPassword[Math.floor(Math.random() * staticPassword.length)];
+    if (excludeDuplicate) {
+      !randomPassword.includes(randomChar) || randomChar == " "
+        ? randomPassword += randomChar
+        : i--;
+    } else {
+      randomPassword += randomChar;
+    }
+  }
+  passwordInput.value = randomPassword;
+};
+
+const updatePassIndicator = () => {
+  passIndicator.id =
+    lengthSlider.value <= 8
+      ? "weak"
+      : lengthSlider.value <= 16
+      ? "medium"
+      : "strong";
+};
+
+const updateSlider = () => {
+  document.querySelector(".pass-length span").innerHTML = lengthSlider.value;
+  generatePassword();
+  updatePassIndicator();
+};
+updateSlider();
+
+const copyPassword = () => {
+  navigator.clipboard.writeText(passwordInput.value);
+  copyIcon.innerHTML = "check";
+  copyIcon.style.color = "#4285f4";
+  setTimeout(() => {
+    copyIcon.innerHTML = "copy_all";
+    copyIcon.style.color = "#707070";
+  }, 1500);
+};
+
+copyIcon.addEventListener("click", copyPassword);
+lengthSlider.addEventListener("input", updateSlider);
+generateBtn.addEventListener("click", generatePassword);
